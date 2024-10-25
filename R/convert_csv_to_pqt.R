479aa01eefb38f4a8cec391c8518eef54acadf1a
Neil Currie <neilcurrie@Neils-MacBook-Air.local>
2024-10-25 14:07:31 +0100

    Add additional arguments to convert_csv_to_pqt: .clean_names to allow passing of the input data frame to janitor::clean_name; overwrite, if TRUE will overwrite file_out

--- R/convert_csv_to_pqt.R
+++ R/convert_csv_to_pqt.R
@@ -7,10 +7,16 @@
 #' @param file_out A character string specifying the path to the output Parquet file.
 #'   Defaults to `NA`, which will use the same path and name as `file_in`,
 #'   replacing the `.csv` extension with `.pqt`.
+#' @param .clean_names logical, determines if input data frame should be passed
+#' to janitor::clean_names
+#' @param overwrite if TRUE will overwrite any existing file_out
 #'
 #' @return This function returns `invisible(NA)`. The output file is saved as a Parquet file.
 #' @importFrom vroom vroom
 #' @importFrom arrow write_parquet
+#' @importFrom janitor clean_names
+#' @importFrom readr cols
+#' @importFrom readr col_character
 #'
 #' @examples
 #' \dontrun{
@@ -23,11 +29,25 @@
 #'
 #' @export
 
-convert_csv_to_pqt <- function (file_in, file_out = NA) {
+convert_csv_to_pqt <- function (file_in, file_out = NA, .clean_names = FALSE,
+                                overwrite = TRUE) {
 
-  df <- vroom(file_in, show_col_types = FALSE, progress = FALSE)
+  df <- vroom(file_in, show_col_types = FALSE, progress = FALSE,
+              col_types = cols(.default = col_character()))
+
+  if (.clean_names) df <- clean_names(df)
 
   if (is.na(file_out)) file_out <- sub("\\.csv$", ".pqt", file_in)
+
+  if (overwrite == FALSE) {
+
+    if (file.exists(file_out)) {
+
+      stop(paste('overwrite == FALSE but file_out exits:', file_out))
+
+    }
+
+  }
 
   write_parquet(df, file_out)
 
